8b0ed9525d0f77c806bfaa305ec684cfb7b4f845
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-04-18 14:46:15 -0500

    Terminado contador

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
 
 function App() {
 
+  const [numClics, setNumClics] = useState(0);
+
   const manejarClic = () => {
-    console.log("Clic");
+    setNumClics(numClics + 1);
   }
 
   const reiniciarContador = () => {
-    console.log("Reiniciar");
+    setNumClics(0);
   }
 
   return (
@@ -22,7 +25,7 @@ function App() {
           alt='Logo de freeCodeCamp'/>
       </div>
       <div className='contenedor-princial'>
-        <Contador numClics='5' />
+        <Contador numClics={numClics} />
         <Boton
           texto = 'Clic' 
           esBotonDeClic={true}
